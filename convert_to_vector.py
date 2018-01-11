613f04e26d5782452c68fd01ee4dcc9915fe5b93
Hoang Cuong <hoangcuong2011@gmail.com>
2018-01-11 16:37:59 -0500

    Update convert_to_vector.py

--- convert_to_vector.py
+++ convert_to_vector.py
@@ -1,41 +1,27 @@
 from encoder import Model
 import time
-
-
 model = Model()
-
 import sys
-
-filename = "test_text"
-
+filename = "def"
 f = open(filename)
-
 fh = open(filename+".vectors","w")
-
 line = f.readline()
 my_dict = {}
+mylist = []
 while line:
     x = line.strip()
-    if x in my_dict:
-    	print("duplicate")
-    	print(x)
-    	vectorOutput = my_dict[x]
-    	fh.write(vectorOutput.strip())
-    	fh.write("\n")
-    	line = f.readline()
-    	continue
-    mylist = []
     mylist.append(x)
-    teXt = model.transform(mylist).reshape(-1)
-    vectorOutput = " ".join(str(x) for x in teXt)
-
-    #for x in teXt:
-    #	fh.write(str(x))
-    #	fh.write(" ")
-    fh.write(vectorOutput.strip())
-    fh.write("\n")
-    my_dict[x] = vectorOutput
-
     line = f.readline()
 f.close()
+
+teXt = model.transform(mylist).reshape(-1)
+print(len(teXt))
+count = 0
+for x in teXt:
+    fh.write(str(x))
+    fh.write(" ")
+    count = count+1
+    if count==4096:
+        count = 0
+        fh.write("\n")
 fh.close()
